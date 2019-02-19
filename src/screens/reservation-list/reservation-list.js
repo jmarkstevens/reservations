85405cf2e20dfd979af12f92ca02244ae07112db
J. Mark Stevens <github@jmarkstevens.com>
2019-02-19 12:28:48 -0800

    hide navigation header

--- src/screens/reservation-list/reservation-list.js
+++ src/screens/reservation-list/reservation-list.js
@@ -4,4 +4,10 @@ import React from 'react'
 
 import ReservationList from '../../containers/reservation-list'
 
-export default () => <ReservationList />
+export default class ReservationListScreen extends React.PureComponent {
+  static navigationOptions = (): { header: null } => ({ header: null })
+
+  render() {
+    return <ReservationList />
+  }
+}
