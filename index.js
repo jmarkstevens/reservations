1cb36e619cfc3d64f80935217b7ea1fb6b369209
J. Mark Stevens <github@jmarkstevens.com>
2019-02-19 11:13:01 -0800

    move app.json

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+// @flow
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent(appName, () => App);
+import App from './src/App'
+import { name as appName } from './src/app.json'
+
+AppRegistry.registerComponent(appName, () => App)
