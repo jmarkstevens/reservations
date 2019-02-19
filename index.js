9e5804de8acf366301854d5d529807abe382c74b
J. Mark Stevens <github@jmarkstevens.com>
2019-02-19 12:27:21 -0800

    linter doesn't like importing json file

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 import { AppRegistry } from 'react-native'
 
 import App from './src/App'
-import { name as appName } from './src/app.json'
+import { name as appName } from './src/app.json' // eslint-disable-line
 
 AppRegistry.registerComponent(appName, () => App)
