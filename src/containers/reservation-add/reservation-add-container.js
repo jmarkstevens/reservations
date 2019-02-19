9333cba4c8bf563012c4d3b27bedc35fc94e7954
J. Mark Stevens <github@jmarkstevens.com>
2019-02-19 11:15:06 -0800

    add containers

---
+++ src/containers/reservation-add/reservation-add-container.js
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react'
+
+import ReservationAdd from '../../components/reservation-add'
+
+export default () => <ReservationAdd />
---
+++ src/containers/reservation-list/reservation-list-container.js
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react'
+
+import ReservationList from '../../components/reservation-list'
+
+export default () => <ReservationList />
