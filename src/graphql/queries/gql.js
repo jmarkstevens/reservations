70ac06602693c8cc520ed2badfd04823bb4cc0d1
J. Mark Stevens <github@jmarkstevens.com>
2019-02-20 08:56:17 -0800

    add createReservation mutation

--- src/graphql/queries/gql.js
+++ src/graphql/queries/gql.js
@@ -12,42 +12,24 @@ export const reservationsQuery = gql`
   }
 `
 
-export const upvoteMutation = gql`
-  mutation upvotePost($postId: Int!, $inVote: Int) {
-    upvotePost(postId: $postId, inVote: $inVote) {
-      id
-      title
-      votes
-      author {
-        lastName
-      }
-    }
-  }
-`
-
-export const upvotedMutation = gql`
-  mutation setUpvotedPost(
-    $upvotePost_id: Int
-    $upvotePost_title: String
-    $upvotePost_votes: Int
-    $upvotePost___typename: String
-    $upvotePost_author_lastName: String
-    $upvotePost_author__typename: String
+export const createReservation = gql`
+  mutation createReservation(
+    $name: String!
+    $hotelName: String!
+    $arrivalDate: String!
+    $departureDate: String!
   ) {
-    setUpvotedPost(id: $id, title: $title, votes: $votes, author: $author, __typename: $__typename)
-      @client
-  }
-`
-
-export const getUpvotedPost = gql`
-  {
-    upvotedPost @client {
+    createReservation(
+      name: $name
+      hotelName: $hotelName
+      arrivalDate: $arrivalDate
+      departureDate: $departureDate
+    ) {
       id
-      title
-      votes
-      author {
-        lastName
-      }
+      name
+      hotelName
+      arrivalDate
+      departureDate
     }
   }
 `
--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,3 +1,3 @@
 // @flow
 
-export { reservationsQuery, upvoteMutation } from './gql'
+export { reservationsQuery, createReservation } from './gql'
