86086da5814fae90028e352bb5fc50082aea7635
J. Mark Stevens <github@jmarkstevens.com>
2019-02-20 11:33:21 -0800

    add createReservation mutation to ReservationAddContainer

--- src/containers/reservation-add/reservation-add-container.js
+++ src/containers/reservation-add/reservation-add-container.js
@@ -1,7 +1,11 @@
 // @flow
 
 import React from 'react'
+import { graphql } from 'react-apollo'
 
+import { createReservation } from '../../graphql/queries'
 import ReservationAdd from '../../components/reservation-add'
 
-export default () => <ReservationAdd />
+const ReservationAddContainer = props => <ReservationAdd {...props} />
+
+export default graphql(createReservation)(ReservationAddContainer)
