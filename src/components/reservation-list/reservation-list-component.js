dd5841abc30d3b3a64bd8c5014abfe721cf7a721
J. Mark Stevens <github@jmarkstevens.com>
2019-02-20 11:32:20 -0800

    use new components

--- src/components/reservation-list/reservation-list-component.js
+++ src/components/reservation-list/reservation-list-component.js
@@ -2,44 +2,21 @@
 
 import React from 'react'
 import {
-  FlatList, SafeAreaView, StyleSheet, Text, View,
+  FlatList, SafeAreaView, StyleSheet,
 } from 'react-native'
+
+import Divider from '../common/divider'
+import Footer from './footer'
+import Header from '../common/header'
+import RenderItem from './render-list-item'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
-  reservationView: {
-    flex: 1,
-    marginBottom: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    marginHorizontal: 10,
-  },
-  textLineLeft: { flex: 5 },
-  textLineRight: { flex: 3 },
+  flatList: { flex: 1 },
 })
-
-const renderItem = ({ item }: { item: any }) => {
-  const name = `Name: ${item.name}`
-  const hotel = `Hotel: ${item.hotelName}`
-  const arrive = `Arrive: ${item.arrivalDate}`
-  const depart = `Depart: ${item.departureDate}`
-  return (
-    <View style={styles.reservationView}>
-      <View style={styles.row}>
-        <Text style={styles.textLineLeft}>{name}</Text>
-        <Text style={styles.textLineRight}>{arrive}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.textLineLeft}>{hotel}</Text>
-        <Text style={styles.textLineRight}>{depart}</Text>
-      </View>
-    </View>
-  )
-}
 
 type Reservation = {
   arrivalDate: string,
@@ -51,13 +28,16 @@ type Reservation = {
 type Props = { data: { reservations: Array<Reservation> } }
 const ReservationList = ({ data: { reservations } }: Props) => (
   <SafeAreaView style={styles.container}>
+    <Header title="Reservations" />
     <FlatList
       data={reservations}
       extraData={reservations}
+      ItemSeparatorComponent={Divider}
       keyExtractor={(item: Reservation) => item.id}
-      renderItem={renderItem}
-      style={{ flex: 1 }}
+      renderItem={({ item }) => <RenderItem item={item} />}
+      style={styles.flatList}
     />
+    <Footer />
   </SafeAreaView>
 )
 
