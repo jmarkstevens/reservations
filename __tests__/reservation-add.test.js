ce051626101aaf304b91237b6523b1a3f9198db2
J. Mark Stevens <github@jmarkstevens.com>
2019-02-21 12:08:26 -0800

    tests for reservation-list and reservation-add

---
+++ __tests__/reservation-add.test.js
@@ -0,0 +1,15 @@
+import 'react-native'
+import React from 'react'
+// import { expect as expectChai } from 'chai'
+import { shallow } from 'enzyme'
+import ReservationAddComponent from '../src/components/reservation-add'
+
+describe('<ReservationAddComponent>', () => {
+  const Component = <ReservationAddComponent />
+
+  const wrapper = shallow(Component)
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
--- __tests__/reservation-list.test.js
+++ __tests__/reservation-list.test.js
@@ -1,15 +1,29 @@
-import 'react-native'
+import { FlatList } from 'react-native'
 import React from 'react'
+import { expect as expectChai } from 'chai'
 import { shallow } from 'enzyme'
 import ReservationListComponent from '../src/components/reservation-list'
 
-describe('<ReservationListComponent>', () => {
-  const Component = <ReservationListComponent data={{ refetch: jest.fn() }} />
+const reservations = [
+  {
+    id: 'testid',
+    name: 'Mark Stevens',
+    hotelName: 'Hilton San Diego',
+    arrivalDate: '02/20/2019',
+    departureDate: '02/21/2019',
+  },
+]
 
-  describe('Structure', () => {
-    it('renders correctly', () => {
-      const wrapper = shallow(Component)
-      expect(wrapper).toMatchSnapshot()
-    })
+describe('<ReservationListComponent>', () => {
+  const Component = <ReservationListComponent data={{ refetch: jest.fn(), reservations }} />
+
+  const wrapper = shallow(Component)
+
+  it('Flatlist length is 1', () => {
+    expectChai(wrapper.find(FlatList).length).to.equal(1)
+  })
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
   })
 })
